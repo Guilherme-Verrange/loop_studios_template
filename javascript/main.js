ed4151c01048983dc0da56e8f19c0574040ab0aa
Guilherme Verrange <60721610+Guilherme-Verrange@users.noreply.github.com>
2022-08-17 18:48:15 -0300

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,14 @@ menuToggle.onclick = function(){
     navBar.classList.toggle('active');
     navList.classList.toggle('active');
 }
+
+const desiredElement = document.getElementById('.navbar'); // elemento alvo
+const pixelsAmount = '5'; // Quantidade de pixels a contar do TOP até definir a cor
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > pixelsAmount) {
+    	desiredElement.classList.add('changeStyle'); // adiciona classe "changeColor"
+    } else {
+      desiredElement.classList.remove('changeStyle'); // remove classe "changeColor"
+    }
+});
